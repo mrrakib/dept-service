811279430fe1dcb5272fa7eb592fed58c83162f9
Rakibul Islam Biplob <biplob.bd33@gmail.com>
2025-04-19 21:58:05 +0600

    adding company crud

---
+++ src/Domain/Entities/Company.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrmBaharu.Domain.Entities;
+public class Company : BaseAuditableEntity
+{
+    public string Name { get; set; } = null!;
+    public string? Address { get; set; }
+}
